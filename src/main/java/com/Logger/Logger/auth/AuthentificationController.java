304c46f4341caf5f8d57c1759746abe781240619
Bahaeddine MLAOUHI <bmlaouhi@sofrecom.fr>
2024-08-19 16:09:49 +0100

    setup complete connection avec le frontend

--- src/main/java/com/Logger/Logger/auth/AuthentificationController.java
+++ src/main/java/com/Logger/Logger/auth/AuthentificationController.java
@@ -1,6 +1,7 @@
 package com.Logger.Logger.auth;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000") // Specify your frontend URL
+
 public class AuthentificationController {
 	private  final authenticationService service;
 
